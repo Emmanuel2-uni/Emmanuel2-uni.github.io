1498f952bc9d3e15c78930ea32dd0d70123b01e8
Emmanuel <132662856+Emmanuel2-uni@users.noreply.github.com>
2024-11-23 23:35:41 +0800

    Update mainLayout.js

--- mainLayout.js
+++ mainLayout.js
@@ -183,7 +183,7 @@ function openDeck(index){
     store_deck_num = index
     console.log(store_deck_num)
     localStorage.setItem("store_index", store_deck_num)
-    console.log("Ping " + sessionStorage.getItem("store_index"))
+    console.log("Ping " + localStorage.getItem("store_index"))
 }
 
 // run
